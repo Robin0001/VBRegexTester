c2ebd094e01e71595a59d40b5b2c77c2e94292d4
Robin Drolet <r_drolet@hotmail.com>
2018-11-16 15:50:46 -0500

    added license

--- VBRegexTester.vb
+++ VBRegexTester.vb
@@ -1,4 +1,6 @@
-﻿Imports System.Text.RegularExpressions
+﻿'<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
+
+Imports System.Text.RegularExpressions
 
 Public Class VBRegexTester
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
